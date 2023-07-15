10b6603117c9ecb20e15378ebe0d0b65f72cd019
김창희 <122184547+kalista00@users.noreply.github.com>
2023-07-15 11:08:49 +0900

    Update reusableWidgets.dart

--- reusableWidgets.dart
+++ reusableWidgets.dart
@@ -5,34 +5,34 @@ class Button extends StatelessWidget{
   final String text;
   final Color bgColor;
   final Color textColor;
-  
+
   const Button({
     super.key,
-    required this.text, 
+    required this.text,
     required this.bgColor,
     required this.textColor,
   });
   @override
   Widget build(BuildContext context){
-    
+
     return Container(
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF1F2123),
-                        borderRadius: BorderRadius.circular(45), //둥글게만들기
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric( 
-                          vertical: 20,
-                          horizontal: 50,
-                        ),
-                        child: Text(
-                          text,
-                          style: TextStyle(
-                            color: textColor,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-      );
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(45), //둥글게만들기
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 50,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
   }
 }
